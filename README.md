ea4224b6716a676c493b31cb3855b3c7d1f075e1
Frederic Heem <frederic.heem@gmail.com>
2017-01-25 21:39:08 +0000

    concatenate an array of string

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ A playground to learn rust by examples.
 # Array
 
   * compute the sum of the array with fold.
-
+  * concatenate an array of string.
+  
 # Date
 
 Examples using the [chrono](https://github.com/lifthrasiir/rust-chrono) crates.
--- src/array_utils.rs
+++ src/array_utils.rs
@@ -1,22 +1,21 @@
 
+
 pub fn sum_array(array : &[i32]) -> i32 {
-  //println!("Sum item {}", array[6]);
   return array.iter().fold(0, |sum, x| sum + x);
 }
 
 #[cfg(test)]
-mod test {
-use super::sum_array;
 
 #[test]
 fn sum_array_test() {
-
-    println!("Hello, world!");
     let array = [1, 2, 5, 6];
     let sum = sum_array(&array);
     println!("Sum {}", sum);
-
 }
 
-
+#[test]
+fn concatenate_array_string() {
+    let array = ["Alice", "Bob"];
+    let concat = array.iter().fold("".to_string(), |sum, value| format!("{} {}", sum, value));
+    println!("array {}", concat);
 }
